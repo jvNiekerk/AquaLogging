09b3d309fb1d0d9076d9dd47fd44d79609514b05
Juan van Niekerk <juanvanniekerk@juans-mbp.lan>
2021-02-10 20:52:13 +0100

    First sensor test with pyseneye library

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello world")
+from pyseneye.sud import SUDevice, Action
+
+d = SUDevice()
+
+d.action(Action.ENTER_INTERACTIVE_MODE)
+s = d.action(Action.SENSOR_READING)
+print(s.ph)
+
+print(s.nh3)
+
+print(s.temperature)
+
+#d.action(Action.LEAVE_INTERACTIVE_MODE)
+#d.close()
+
