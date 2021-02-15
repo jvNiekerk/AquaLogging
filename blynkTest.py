a47315081e9bc9f2ccc3d4d85e068c051c9383d0
Juan van Niekerk <juanvanniekerk@juans-mbp.lan>
2021-02-15 19:06:55 +0100

    Adding blynk interface

---
+++ blynkTest.py
@@ -0,0 +1,23 @@
+
+import blynklib
+import random
+
+BLYNK_AUTH = 'Fe1AeNSUHx8jigmocA9msJQHq0_nzZP8'
+
+# initialize blynk
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
+
+
+# register handler for virtual pin V11 reading
+@blynk.handle_event('read V11')
+def read_virtual_pin_handler(pin):
+    print(READ_PRINT_MSG.format(pin))
+    blynk.virtual_write(pin, random.randint(0, 255))
+
+###########################################################
+# infinite loop that waits for event
+###########################################################
+while True:
+    blynk.run()
